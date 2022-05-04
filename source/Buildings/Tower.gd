ea5f4f35f009ec1ef82150e64ea39dfb26afad96
Alexey-788 <alexey.sheblykin1@gmail.com>
2022-05-04 19:52:44 +0300

    test level

--- source/Buildings/Tower.gd
+++ source/Buildings/Tower.gd
@@ -10,11 +10,16 @@ var health = MAX_HEALTH
 var start_health_line_with = 16
 var is_alive = true
 
+var damage_mutex = Mutex.new()
+
 func get_damage(value):
+	damage_mutex.lock()
+	print("damage")
 	health_line.mesh.size.x = float(health)/MAX_HEALTH * start_health_line_with
 	health -= 10
 	if health <= 0:
 		destroy()
+	damage_mutex.unlock()
 		
 func destroy():
 	health_line.mesh.size.x = 0
